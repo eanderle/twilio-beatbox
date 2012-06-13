d1a4a28ef6bce6c7b19f95ab4d2c7b96426e0423
Eric Anderle <anderle@twilio.com>
2012-06-12 23:21:59 -0700

    record voice, play it back

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 import os
 import twilio.twiml as twiml
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def beatbox():
     r = twiml.Response()
-    r.say("Welcome to Twilio Beatbox! Record a loop after the ding. Ding")
-    r.record(action='/record_handler')
+    r.say("Welcome to Twilio Beatbox! Record a loop after the ding. Press pound when finished. Ding")
+    r.record(action='/record_handler', method='GET', playBeep=False, finishOnKey='#')
+    return str(r)
+
+@app.route('/record_handler')
+def record_handler():
+    r = twiml.Response()
+    r.play(request.values.get('RecordingUrl'))
     return str(r)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.debug = True
     app.run(host='0.0.0.0', port=port)
