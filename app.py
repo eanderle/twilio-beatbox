99f630466f686b1437edc3f1a86fd24e6f00d331
Eric Anderle <anderle@twilio.com>
2012-06-13 03:08:13 -0700

    fixed some derp with filenames

--- app.py
+++ app.py
@@ -23,27 +23,29 @@ def beatbox():
 def record_handler():
     global song
     r = twiml.Response()
-    filename = urllib.urlretrieve(request.values.get('RecordingUrl'))[0]
+
+    flask_dir = os.path.dirname(os.path.abspath(__file__))
+    static_dir = os.path.join(flask_dir, "static/")
+    filename = os.path.join(static_dir, 'lol')
+    urllib.urlretrieve(request.values.get('RecordingUrl'), filename)
     tmp_file = wave.open(filename)
     s = tmp_file.readframes(NUM_FRAMES)
+    os.remove(filename)
     if len(s) == 0:
         s = '\x00' * NUM_FRAMES
     elif len(s) < NUM_FRAMES:
         s = (s * ((NUM_FRAMES / len(s)) + 1))
     song = audioop.add(s[:NUM_FRAMES], song, 2)
 
-    flask_dir = os.path.dirname(os.path.abspath(__file__))
-    static_dir = os.path.join(flask_dir, "static/")
-
     fuu_name = hashlib.sha224(song).hexdigest() + '.wav'
-    fuu = wave.open(os.path.join(static_dir, fuu_name), 'w')
+    fuu_full_path = os.path.join(static_dir, fuu_name)
+    fuu = wave.open(fuu_full_path, 'w')
     fuu.setnchannels(1)
     fuu.setsampwidth(2)
     fuu.setframerate(8000)
     fuu.writeframes(song)
     fuu.close()
     r.play(url_for('static', filename=fuu_name))
-    os.remove(fuu_name)
     r.say('Press 1 to record another track or 2 to finish')
     r.gather(method='GET', numDigits=1, action='/user_option')
     return str(r)
